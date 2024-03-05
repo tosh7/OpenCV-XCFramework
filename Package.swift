e9252999222e1ec5d748535d5c37a7225e960ef7
tosh7 <zlia.6.lj.425@gmail.com>
2024-03-05 11:26:25 +0900

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+import PackageDescription
+
+let package = Package(
+  name: "opencv2", platforms: [.iOS(.v11)],
+  products: [
+	.library(name: "opencv2", targets: ["opencv2"]),
+  ],
+  dependencies: [],
+  targets: [
+	.binaryTarget(
+	  name: "opencv2", url: "https://github.com/tosh7/OpenCV-XCFramework/releases/download/4.9.0/opencv2.xcframework.zip",
+	  checksum: "13e0b52648a04a233b9ca4fcd8282dda9bf641bdcde21a27cc42a685ea9a0f36"
+	),
+  ]
+)
